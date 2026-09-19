8c3b1f2d33fc1019988b5d2859edb772e6eee078
Anshika02006 <anshikaagrawa2006@gmail.com>
2026-09-19 15:04:57 +0530

    Initial commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,9 @@
+import Whiteboard from "./components/Whiteboard";
+
+export default function App() {
+  return (
+    <div style={{ height: "100vh" }}>
+      <Whiteboard roomCode="ABCD12" />
+    </div>
+  );
+}
